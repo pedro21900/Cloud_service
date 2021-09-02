6102bca6ff13693b7e3557e4276d75a9bc131aa1
Pedro Lenonn <pedro21900@hotmail.com>
2021-09-02 12:11:56 -0300

    fist commit

---
+++ src/main/java/labsdio/productcatalog/domain/Product.java
@@ -0,0 +1,17 @@
+/*
+
+package labsdio.productcatalog.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Data
+@Document(indexName = "product")
+public class Product {
+    @Id
+    private Long id;
+    private String name;
+    private Integer amount;
+}
+*/
---
+++ src/main/java/labsdio/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+/*
+
+package labsdio.productcatalog.repository;
+
+import labsdio.productcatalog.domain.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+}*/
