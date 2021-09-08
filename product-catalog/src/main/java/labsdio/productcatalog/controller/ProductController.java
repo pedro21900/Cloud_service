d50647827cfe79ff891618098aaba4d4e704cfbb
Pedro Lenonn <pedro21900@hotmail.com>
2021-09-08 11:56:55 -0300

    Feat: service-discovery gateway

--- product-catalog/src/main/java/labsdio/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/labsdio/productcatalog/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/product")
 @AllArgsConstructor
 public class ProductController {
 
